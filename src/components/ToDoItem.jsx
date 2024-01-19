83c557d6824e7ab0d32ee5fc4bd2f7cccc752a47
ParvathyAjRaj <paruvathy99@gmail.com>
2024-01-19 16:11:40 +0530

    cleaned up

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -4,11 +4,6 @@ function ToDo(props){
 
     const [isDone,setIsDone] = useState(false);
 
-    // function handleDelete(){
-    //     console.log("in todo comp -> asked to delete");
-
-    // }
-    
     function handleClick(){
         setIsDone(!isDone);
     }    
@@ -18,7 +13,6 @@ function ToDo(props){
             <li className="list" style={{textDecoration: isDone?"line-through":"none"}} key={props.id}>{props.todo}</li>
             <button className="done" onClick={handleClick} key={props.id}>Done</button>
             <button className="delete" onClick={()=>{props.onChecked(props.index)}} key={props.id}>Delete</button>
-            {/* <button onClick={handleDelete}>Delete</button> */}
         </div>
     )
 }
